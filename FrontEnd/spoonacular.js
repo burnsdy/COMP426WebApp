a986bbf896eb6b0ed564e07b6c67592c99cd726e
Dylan Burns <burnsdy@live.unc.edu>
2019-12-11 16:41:54 -0500

    fixed spoonacular to work with axios

--- FrontEnd/spoonacular.js
+++ FrontEnd/spoonacular.js
@@ -1,19 +1,42 @@
-export async function recipeSearch(event) {
+async function requestID(event) {
     event.preventDefault();
     const result = await axios({
         method: 'get',
         url: 'https://api.spoonacular.com/recipes/search?apiKey=196f5984b339460580d5dfca1b04e7be&query=' + $("input[id=recipeVal]").val() + '&number=1&limitLicense=true&instructionsRequired=true', // NUMBER IS SET TO 1 FOR NOW, CHANGE TO 10
     });
+    return result;
+}
 
-    // for(let i=0; i<result.length; i++) {
-    //     if (result[i].data.id == event.target.id) {
-    //         oldTweet = result[i].data;
-    //     }
-    // }
+async function requestInfo(event) {
+    event.preventDefault();
+    const result = await axios({
+        method: 'get',
+        url: 'https://api.spoonacular.com/recipes/search?apiKey=196f5984b339460580d5dfca1b04e7be&query=' + $("input[id=recipeVal]").val() + '&number=1&limitLicense=true&instructionsRequired=true', // NUMBER IS SET TO 1 FOR NOW, CHANGE TO 10
+    });
+    return result;
+}
 
+async function getId(event) {
+    let result = await requestID(event);
+    let response = [];
+    response.push(result.data);
+    let recipe = response[0].results;
+    let id = recipe[0].id;
+    return id;
+    // requestID(event).then(result => {
+    //     let response = [];
+    //     response.push(result.data);
+    //     let recipe = response[0].results;
+    //     let id = recipe[0].id;
+    //     return id;
+    // }).catch(error => {
+    //     console.log("Unable to fetch searched")
+    // });
+}
 
-    let recipes = result["results"];
-    alert(recipes[0].title);
+async function recipeSearch(event) {
+    let id = await getId(event);
+    alert(id);
 }
 
 $(document).on('click', '#recipeSearch', recipeSearch);
