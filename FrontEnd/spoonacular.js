9d9610209ad7b8ee1f514f0c06e1e74e09c084ed
Dylan Burns <burnsdy@live.unc.edu>
2019-12-11 18:38:46 -0500

    spoonacular returns json array

--- FrontEnd/spoonacular.js
+++ FrontEnd/spoonacular.js
@@ -2,16 +2,7 @@ async function requestID(event) {
     event.preventDefault();
     const result = await axios({
         method: 'get',
-        url: 'https://api.spoonacular.com/recipes/search?apiKey=196f5984b339460580d5dfca1b04e7be&query=' + $("input[id=recipeVal]").val() + '&number=1&limitLicense=true&instructionsRequired=true', // NUMBER IS SET TO 1 FOR NOW, CHANGE TO 10
-    });
-    return result;
-}
-
-async function requestInfo(event) {
-    event.preventDefault();
-    const result = await axios({
-        method: 'get',
-        url: 'https://api.spoonacular.com/recipes/search?apiKey=196f5984b339460580d5dfca1b04e7be&query=' + $("input[id=recipeVal]").val() + '&number=1&limitLicense=true&instructionsRequired=true', // NUMBER IS SET TO 1 FOR NOW, CHANGE TO 10
+        url: 'https://api.spoonacular.com/recipes/search?apiKey=196f5984b339460580d5dfca1b04e7be&query=' + $("input[id=recipeVal]").val() + '&number=5&limitLicense=true&instructionsRequired=true', // NUMBER IS SET TO 1 FOR NOW, CHANGE TO 10
     });
     return result;
 }
@@ -21,22 +12,54 @@ async function getId(event) {
     let response = [];
     response.push(result.data);
     let recipe = response[0].results;
-    let id = recipe[0].id;
-    return id;
-    // requestID(event).then(result => {
-    //     let response = [];
-    //     response.push(result.data);
-    //     let recipe = response[0].results;
-    //     let id = recipe[0].id;
-    //     return id;
-    // }).catch(error => {
-    //     console.log("Unable to fetch searched")
-    // });
+    let ids = [recipe[0].id, recipe[1].id, recipe[2].id, recipe[3].id, recipe[4].id]
+    return ids; // array of 5 ids
 }
 
-async function recipeSearch(event) {
-    let id = await getId(event);
-    alert(id);
+async function requestInfo(id) {
+    const result = await axios({
+        method: 'get',
+        url: 'https://api.spoonacular.com/recipes/' + id + '/information/?apiKey=196f5984b339460580d5dfca1b04e7be'
+    });
+    return result;
 }
 
-$(document).on('click', '#recipeSearch', recipeSearch);
+async function getRecipe(id) {
+    let result = await requestInfo(id);
+    let response = [];
+    response.push(result.data);
+    let name = response[0].title;
+    let imageurl = response[0].image;
+    let ingredients = [];
+    for(let i=0; i<response[0].extendedIngredients.length; i++) {
+        ingredients.push(response[0].extendedIngredients[i].originalString);
+    }
+    let instructions = response[0].instructions;
+    let obj = {
+        id: id,
+        image: imageurl,
+        name: name,
+        ingredients: ingredients,
+        instructions: instructions
+    }
+    let json = JSON.stringify(obj);
+    return json;
+}
+
+async function buildHTML(array) {
+
+}
+
+async function recipeHandler(event) {
+    let recipesjson = [];
+    let ids = await getId(event);
+    for(let i=0; i<5; i++) {
+        let json = await getRecipe(ids[i]);
+        recipesjson.push(json);
+    }
+    console.log(recipesjson);
+    let html = buildHTML(recipesjson);
+    // Add html to page below search bar
+}
+
+$(document).on('click', '#recipeSearch', recipeHandler);
