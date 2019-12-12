6de69dc8bbfe6d1692595292cbe34ff7d3845949
Dylan Burns <burnsdy@live.unc.edu>
2019-12-12 04:57:21 -0500

    half of user functionality

--- FrontEnd/functionality/backendStuff.js
+++ FrontEnd/functionality/backendStuff.js
@@ -29,8 +29,41 @@ async function getUser() {
 
 async function loadUser() {
     let result = await getUser();
-    console.log("test");
-    console.log(result);
+    let objs = result.data.result;
+    let ids = Object.keys(objs);
+    let html = `<div id="recipes">`;
+    for(let i=0; i<ids.length; i++) {
+        let id = ids[i];
+        let name = objs[id].name;
+        let ingArray = objs[id].ingredients;
+        let ingredients = `<ul>`;
+        for(let k=0; k<ingArray.length; k++) {
+            ingredients = ingredients + `
+                <li>${ingArray[k]}</li>
+            `;
+        }
+        ingredients = ingredients + `</ul>`;
+        let instructions = objs[id].instructions;
+        let render = `
+            <div class="recipebox">
+                <div class="inner">
+                    <h2>${name}</h2>
+                    <hr>
+                    <p>${ingredients}</p>
+                    <p>${instructions}<p>
+                    <button id=${id} class="edit">Edit</button>
+                    <button id=${id} class="delete">Delete</button>
+                </div>
+            </div>
+        `;
+        html = html + render;
+        console.log(i);
+        if (i>4) {
+            break;
+        }
+    }
+    html = html + `</div>`;
+    $('#recipes').replaceWith(html);
 }
 
 async function loadPrivate() {
@@ -169,7 +202,13 @@ async function stringToHash(string) {
 } 
 
 async function delRecipe() {
-    
+    const recipeID = event.target.id;
+    let obj = await getRecipe(recipeID);
+}
+
+async function editRecipe() {
+    const recipeID = event.target.id;
+    let obj = await getRecipe(recipeID);
 }
 
 $(document).on('click', '.save', saveRecipe);
